738c4621f1c000ca0bc7406217c87840f99d74d9
CSLP <pierre.castel@cnieg.fr>
2025-07-21 01:06:41 +0200

    add vaccination tests

--- src/test/java/Day6Test.java
+++ src/test/java/Day6Test.java
@@ -37,7 +37,6 @@ class Day6Test {
             """
                 Deny citizens of Antegria
                 """);
-
     }
 
     @Test
@@ -179,7 +178,6 @@ class Day6Test {
                 EXP: 1984.11.01
                 NAME: Wagner, Khalid
                 """);
-
         entrant.put("grant_of_asylum",
             """
                 NAME: Wagner, Khalid
@@ -194,4 +192,83 @@ class Day6Test {
         assertEquals("Cause no trouble.", inspector.inspect(entrant));
     }
 
+    @Test
+    void validForeigner_TargetedByVaccinationByNation() {
+        inspector.receiveBulletin(
+            """
+                Allow citizens of Antegria
+                Deny citizens of Kolechia
+                Citizens of United Federation, Kolechia require polio vaccination
+                """);
+
+        Map<String, String> entrant = new HashMap<>();
+        entrant.put("passport",
+            """
+                NATION: Antegria
+                DOB: 1919.02.10
+                SEX: M
+                ISS: Mergerous
+                ID#: S9GRA-KO17I
+                EXP: 1984.11.01
+                NAME: Wagner, Khalid
+                """);
+        entrant.put("grant_of_asylum",
+            """
+                NAME: Wagner, Khalid
+                NATION: Antegria
+                ID#: S9GRA-KO17I
+                DOB: 1919.02.10
+                HEIGHT: 176.0cm
+                WEIGHT: 84.0kg
+                EXP: 1984.02.26
+                """);
+        entrant.put("certificate_of_vaccination",
+            """
+                NAME: Wagner, Khalid
+                ID#: S9GRA-KO17I
+                VACCINES: HPV, polio, cowpox
+                """);
+
+        assertEquals("Cause no trouble.", inspector.inspect(entrant));
+    }
+
+    @Test
+    void invalidForeigner_TargetedByVaccinationForForeigners() {
+        inspector.receiveBulletin(
+            """
+                Allow citizens of Antegria
+                Deny citizens of Kolechia
+                Foreigners require polio vaccination
+                """);
+
+        Map<String, String> entrant = new HashMap<>();
+        entrant.put("passport",
+            """
+                NATION: Antegria
+                DOB: 1919.02.10
+                SEX: M
+                ISS: Mergerous
+                ID#: S9GRA-KO17I
+                EXP: 1984.11.01
+                NAME: Wagner, Khalid
+                """);
+        entrant.put("grant_of_asylum",
+            """
+                NAME: Wagner, Khalid
+                NATION: Antegria
+                ID#: S9GRA-KO17I
+                DOB: 1919.02.10
+                HEIGHT: 176.0cm
+                WEIGHT: 84.0kg
+                EXP: 1984.02.26
+                """);
+        entrant.put("certificate_of_vaccination",
+            """
+                NAME: Wagner, Khalid
+                ID#: S9GRA-KO17I
+                VACCINES: HPV, cowpox
+                """);
+
+        assertEquals("Entry denied: missing required vaccination.", inspector.inspect(entrant));
+    }
 }
